d34fe453d17e67bc09dcbe46574b77598a435666
Lokesh Gupta <howtodoinjava@gmail.com>
2020-05-24 21:50:16 +0530

    Initial commit

---
+++ spring-boot-kafka-app/src/main/java/com/howtodoinjava/kafka/demo/SpringBootKafkaAppApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka-app/src/test/java/com/howtodoinjava/kafka/demo/SpringBootKafkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.kafka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
