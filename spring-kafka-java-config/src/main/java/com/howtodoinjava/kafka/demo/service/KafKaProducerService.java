099dfb144f064d783e670b9f64600d26b67ee97b
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-06-26 13:05:37 +0530

    Upgraded to Spring Boot 3

--- spring-kafka-java-config/src/main/java/com/howtodoinjava/kafka/demo/service/KafKaProducerService.java
+++ spring-kafka-java-config/src/main/java/com/howtodoinjava/kafka/demo/service/KafKaProducerService.java
@@ -1,5 +1,6 @@
 package com.howtodoinjava.kafka.demo.service;
 
+import com.howtodoinjava.kafka.demo.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,68 +8,58 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
-import com.howtodoinjava.kafka.demo.model.User;
+import java.util.concurrent.CompletableFuture;
 
 @Service
-public class KafKaProducerService 
-{
-	private static final Logger logger = 
-			LoggerFactory.getLogger(KafKaProducerService.class);
-	
-	//1. General topic with string payload
-	
-	@Value(value = "${general.topic.name}")
-    private String topicName;
-	
-	@Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+public class KafKaProducerService {
+  private static final Logger logger =
+      LoggerFactory.getLogger(KafKaProducerService.class);
 
-	//2. Topic with user object payload
-    
-    @Value(value = "${user.topic.name}")
-    private String userTopicName;
-    
-    @Autowired
-    private KafkaTemplate<String, User> userKafkaTemplate;
-	
-	public void sendMessage(String message) 
-	{
-		ListenableFuture<SendResult<String, String>> future 
-			= this.kafkaTemplate.send(topicName, message);
-		
-		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
-            @Override
-            public void onSuccess(SendResult<String, String> result) {
-            	logger.info("Sent message: " + message 
-            			+ " with offset: " + result.getRecordMetadata().offset());
-            }
+  //1. General topic with string payload
 
-            @Override
-            public void onFailure(Throwable ex) {
-            	logger.error("Unable to send message : " + message, ex);
-            }
-       });
-	}
-	
-	public void saveCreateUserLog(User user) 
-	{
-		ListenableFuture<SendResult<String, User>> future 
-			= this.userKafkaTemplate.send(userTopicName, user);
-		
-		future.addCallback(new ListenableFutureCallback<SendResult<String, User>>() {
-            @Override
-            public void onSuccess(SendResult<String, User> result) {
-            	logger.info("User created: " 
-            			+ user + " with offset: " + result.getRecordMetadata().offset());
-            }
+  @Value(value = "${general.topic.name}")
+  private String topicName;
 
-            @Override
-            public void onFailure(Throwable ex) {
-            	logger.error("User created : " + user, ex);
-            }
-       });
-	}
+  @Autowired
+  private KafkaTemplate<String, String> kafkaTemplate;
+
+  //2. Topic with user object payload
+
+  @Value(value = "${user.topic.name}")
+  private String userTopicName;
+
+  @Autowired
+  private KafkaTemplate<String, User> userKafkaTemplate;
+
+  public void sendMessage(String message) {
+    CompletableFuture<SendResult<String, String>> future
+        = this.kafkaTemplate.send(topicName, message);
+
+    future.whenComplete((result, throwable) -> {
+
+      if (throwable != null) {
+        // handle failure
+        logger.error("Unable to send message : " + message, throwable);
+      } else {
+        // handle success
+        logger.info("Sent message: " + message + " with offset: " + result.getRecordMetadata().offset());
+      }
+    });
+  }
+
+  public void saveCreateUserLog(User user) {
+    CompletableFuture<SendResult<String, User>> future
+        = this.userKafkaTemplate.send(userTopicName, user);
+
+    future.whenComplete((result, throwable) -> {
+      if (throwable != null) {
+        // handle failure
+        logger.error("User created : " + user, throwable);
+      } else {
+        // handle success
+        logger.info("User created: " + user + " with offset: " + result.getRecordMetadata().offset());
+      }
+    });
+  }
 }
